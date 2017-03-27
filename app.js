f568d0d3940b55e5b9c7ecf1aa5a0910c3af0743
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2017-03-27 15:43:02 +0100

    Use expressMetrics

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var expressMetrics = require('express-metrics');
 var bodyparser = require('body-parser');
 var connection = require('./connection');
 var routes = require('./routes');
@@ -6,12 +7,20 @@ var routes = require('./routes');
 var app = express();
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
-app.use(express.static('front-end'))
+app.use(express.static('front-end'));
 
+// TODO: inject config / environment variable
+var serverPort = 8000;
+var metricsPort = 8091;
+
+// start a metrics server
+app.use(expressMetrics({
+  port: metricsPort
+}));
 
 connection.init();
 routes.configure(app);
 
-var server = app.listen(8000, function() {
+var server = app.listen(serverPort, function() {
   console.log('Server listening on port ' + server.address().port);
 });
